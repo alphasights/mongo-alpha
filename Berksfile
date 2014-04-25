57dfbb8fed6f04a2b3ff67569fcdc79270ab2677
Michael Westbom <michael.westbom@alphasights.com>
2014-04-25 10:02:47 -0400

    Initial commit; with some cookbooks

---
+++ Berksfile
@@ -0,0 +1,5 @@
+source "https://api.berkshelf.com"
+
+metadata
+
+cookbook 'mongodb', git: 'git://github.com/edelight/chef-mongodb.git'
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'mongo-alpha'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures mongo-alpha'
+long_description 'Installs/Configures mongo-alpha'
+version          '0.1.0'
+
